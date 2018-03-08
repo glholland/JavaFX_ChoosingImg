e7769101fff64707fd878074cb5d795e7f99dca6
Garrett Holland <igotsol@live.com>
2018-03-08 16:49:29 -0500

    Cleaned premade code.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,16 +1,17 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        FlowPane root = new FlowPane();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
